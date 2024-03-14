807236d807da8189330783f31277c70919a7dbf4
Manthan B T <163422807+manthan-bt@users.noreply.github.com>
2024-03-14 19:23:48 +0530

    CADD Mini Project.sv

--- CADD Mini Project.sv
+++ CADD Mini Project.sv
@@ -1,3 +1,4 @@
+//Main code:
 module matrix_inversion(
     input logic signed [7:0] a11, a12, a13,
     input logic signed [7:0] a21, a22, a23,
@@ -16,5 +17,20 @@ module matrix_inversion(
   assign b31 = ((a21 * a32 - a22 * a31) / determinant);
   assign b32 = ((a12 * a31 - a11 * a32) / determinant);
   assign b33 = ((a11 * a22 - a12 * a21) / determinant);
+  end
+endmodule
+
+//Test Bench Code
+module matrix_inversion_tb;
+logic [7:0]a11,a12,a13,a21,a22,a23,a31,a32,a33,b11,b12,b13,b21,b22,b23,b31,b32,b33;logic [23:0] determinant;
+  // Instantiate the module to be tested
+  matrix_inversion dut ( a11, a12, a13, a21, a22, a23, a31, a32, a33, b11, b12, b13, b21, b22, b23, b31, b32, b33,determinant);
+
+  initial begin
+    #10 a11 = 1; a12 = 0; a13 = 1;
+    a21 = 0; a22 = 1; a23 = 0;
+    a31 = 1; a32 = 0; a33 = 1;
+    #10;
+  $finish;
   end
 endmodule
